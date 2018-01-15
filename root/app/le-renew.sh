10167494cdd335cade14454ef58f2a79fadc4a1b
aptalca <aptalca@users.noreply.github.com>
2018-01-15 12:50:13 -0500

    Allow DNS validation for renewal

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -5,4 +5,11 @@ echo
 echo "<------------------------------------------------->"
 echo "cronjob running on "$(date)
 echo "Running certbot renew"
-certbot -n renew --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+. /config/donoteditthisfile.conf
+if [ "$ORIGDNSVAL" = "true" ]; then
+  echo "Performing DNS validation"
+  cetbot -n renew --manual --manual-auth-hook /config/authenticator.sh --manual-cleanup-hook /config/cleanup.sh --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+else
+  echo "Performing http/s validation"
+  certbot -n renew --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+fi
